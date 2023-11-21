8b5283df24c41c150552e68045a643a7c42c6fd2
ismail wafid <ismailwafid.contact@gmail.com>
2023-11-21 05:00:14 +0100

    some changes

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const boxLeft = document.querySelector(".box-login");
-    const boxRight = document.querySelector(".box-sign-up");
-    const signUpLink = document.querySelector(".sign-up-btn");
+    const container = document.querySelector(".container");
 
-    signUpLink.addEventListener('click', function () {
-        boxLeft.classList.toggle('d-none');
-        boxRight.classList.toggle('d-none');
+    container.addEventListener('click', function (event) {
+        const signUpLink = event.target.closest('.sign-up-btn');
+
+        if (signUpLink) {
+            const boxLeft = document.querySelector(".box-login");
+            const boxRight = document.querySelector(".box-sign-up");
+
+            boxLeft.classList.toggle('d-none');
+            boxRight.classList.toggle('d-none');
+        }
     });
 });
